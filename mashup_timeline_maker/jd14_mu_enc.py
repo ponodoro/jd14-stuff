9e29def86b63acd374c0eddeefb4b61f943eb81f
JackLSummer15 <62530870+JackLSummer15@users.noreply.github.com>
2022-01-06 11:30:55 -0500

    Add files via upload

---
+++ mashup_timeline_maker/jd14_mu_enc.py
@@ -0,0 +1,61 @@
+import struct, json, os
+from unidecode import unidecode
+
+try:
+    os.mkdir('input')
+    os.mkdir('output')
+
+except:
+    pass
+
+codename=input("Enter a map to encrypt: ")
+codenamelow=codename.lower()
+
+with open(codenamelow+"mu.tpl.ckd") as f:
+    tape=json.load(f)
+
+enc=open("ENCRYPTED_"+codenamelow+"mu.tpl.ckd","wb")
+tapecount=len(tape["COMPONENTS"][0]["BlockDescriptorVector"])
+print(tapecount)
+tape_version=int(tapecount*2000)
+#header
+enc.write(b'\x00\x00\x00\x01'+struct.pack(">I",tape_version)+b'\x1B\x85\x7B\xCE\x00\x00\x00\xAC\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x5B\x64\x8E\x44\x00\x00\x00\x2C')
+if tape["COMPONENTS"][0]["IsMashUp"]==1:
+    enc.write(b'\x00\x00\x00\x01\x00\x00\x00\x00')
+elif tape["COMPONENTS"][0]["IsPartyMaster"]==1:
+    enc.write(b'\x00\x00\x00\x00\x00\x00\x00\x01')
+else:
+    enc.write(b'\x00\x00\x00\x00\x00\x00\x00\x00')
+
+#blockflow tapes
+tapeclips=struct.pack(">I",tapecount)
+for blockflow in tape["COMPONENTS"][0]["BlockDescriptorVector"]:
+        tapeclips+=b'\x00\x00\x01\xA8'
+        tapeclips+=b'\x00\x00\x01\x94'
+        basemapname=unidecode(blockflow["BaseBlock"]["songName"])
+        tapeclips+=struct.pack(">I",len(basemapname))+basemapname.encode()
+        tapeclips+=struct.pack(">I",blockflow["BaseBlock"]["jdVersion"])
+        tapeclips+=struct.pack(">i",blockflow["BaseBlock"]["frstBeat"])
+        tapeclips+=struct.pack(">i",blockflow["BaseBlock"]["lastBeat"])
+        tapeclips+=struct.pack(">I",blockflow["BaseBlock"]["songSwitch"])
+        tapeclips+=struct.pack(">f",blockflow["BaseBlock"]["videoCoachOffset"][0])
+        tapeclips+=struct.pack(">f",blockflow["BaseBlock"]["videoCoachOffset"][1])
+        tapeclips+=struct.pack(">f",blockflow["BaseBlock"]["videoCoachScale"])
+        basemovename=unidecode(blockflow["BaseBlock"]["danceStepName"])
+        tapeclips+=struct.pack(">I",len(basemovename))+basemovename.encode()
+        tapeclips+=struct.pack(">I",len(blockflow["AlternativeBlocks"]))
+        for altblock in blockflow["AlternativeBlocks"]:
+            tapeclips+=b'\x00\x00\x01\x94'
+            altmapname=unidecode(altblock["songName"])
+            tapeclips+=struct.pack(">I",len(altmapname))+altmapname.encode()
+            tapeclips+=struct.pack(">I",altblock["jdVersion"])
+            tapeclips+=struct.pack(">i",altblock["frstBeat"])
+            tapeclips+=struct.pack(">i",altblock["lastBeat"])
+            tapeclips+=struct.pack(">I",altblock["songSwitch"])
+            tapeclips+=struct.pack(">f",altblock["videoCoachOffset"][0])
+            tapeclips+=struct.pack(">f",altblock["videoCoachOffset"][1])
+            tapeclips+=struct.pack(">f",altblock["videoCoachScale"])
+            altmovename=unidecode(altblock["danceStepName"])
+            tapeclips+=struct.pack(">I",len(altmovename))+altmovename.encode()
+
+enc.write(tapeclips)
---
+++ mashup_timeline_maker/jd14_pm_dec.py
@@ -0,0 +1,107 @@
+import struct, json, os
+
+try:
+    os.mkdir('input')
+    os.mkdir('output')
+
+except:
+    pass
+
+codename=input("Enter a map to decrypt: ")
+codenamelow=codename.lower()
+
+f = open("input/"+codenamelow+"pm.tpl.ckd","rb")
+
+mutpl={
+    "__class": "Actor_Template",
+    "WIP": 0,
+    "LOWUPDATE": 0,
+    "UPDATE_LAYER": 0,
+    "PROCEDURAL": 0,
+    "STARTPAUSED": 0,
+    "FORCEISENVIRONMENT": 0,
+    "COMPONENTS": [{
+            "__class": "JD_BlockFlowTemplate",
+            "IsMashUp": 0,
+            "IsPartyMaster": 1,
+            "BlockDescriptorVector": []}]}
+
+#header
+f.read(64)
+
+#info
+maptapes = struct.unpack('>I', f.read(4))[0]
+for x in range(maptapes):
+    #base input
+    baseclassentry = f.read(8)
+
+    len_basesongname = struct.unpack('>I', f.read(4))[0]
+
+    basesongname = f.read(len_basesongname).decode("utf-8")
+
+    basejdversion = struct.unpack('>I', f.read(4))[0]
+
+    basefirstbeat = struct.unpack('>i', f.read(4))[0]
+
+    baselastbeat = struct.unpack('>I', f.read(4))[0]
+
+    basesongswitch = struct.unpack('>I', f.read(4))[0]
+
+    basevideooff1 = struct.unpack('>f', f.read(4))[0]
+
+    basevideooff2 = struct.unpack('>f', f.read(4))[0]
+
+    basevideoscale = struct.unpack('>f', f.read(4))[0]
+
+    len_basemovename=struct.unpack('>I', f.read(4))[0]
+
+    basemovename=f.read(len_basemovename).decode("utf-8")
+
+    baseblockclip={"__class":"JD_BlockDescriptor","songName": basesongname,"jdVersion": basejdversion,"frstBeat": basefirstbeat,"lastBeat": baselastbeat,"songSwitch": basesongswitch,"videoCoachOffset": [basevideooff1, basevideooff2],"videoCoachScale": basevideoscale,"danceStepName": basemovename}
+
+    blockreplaceclip={
+    "__class": "JD_BlockReplacements",
+    "BaseBlock": baseblockclip,
+    "AlternativeBlocks": []
+    }
+
+    #replacing parts
+
+    altblock = struct.unpack('>I', f.read(4))[0]
+    for x in range(altblock):
+
+        altclassentry = f.read(4)
+
+        len_altsongname = struct.unpack('>I', f.read(4))[0]
+
+        altsongname = f.read(len_altsongname).decode("utf-8")
+
+        altjdversion = struct.unpack('>I', f.read(4))[0]
+
+        altfirstbeat = struct.unpack('>i', f.read(4))[0]
+
+        altlastbeat = struct.unpack('>I', f.read(4))[0]
+
+        altsongswitch = struct.unpack('>I', f.read(4))[0]
+
+        altvideooff1 = struct.unpack('>f', f.read(4))[0]
+
+        altvideooff2 = struct.unpack('>f', f.read(4))[0]
+
+        altvideoscale = struct.unpack('>f', f.read(4))[0]
+
+        len_altmovename=struct.unpack('>I', f.read(4))[0]
+
+        altmovename=f.read(len_altmovename).decode("utf-8")
+
+        altblockclip={"__class":"JD_BlockDescriptor","songName":altsongname,"jdVersion": altjdversion,"frstBeat":altfirstbeat,"lastBeat":altlastbeat,"songSwitch":altsongswitch,"videoCoachOffset":[altvideooff1,altvideooff2],"videoCoachScale":altvideoscale,"danceStepName":altmovename}
+
+        blockreplaceclip["AlternativeBlocks"].append(altblockclip)
+
+    mutpl["COMPONENTS"][0]["BlockDescriptorVector"].append(blockreplaceclip)
+
+f.close()
+
+mudec = open("output/"+codenamelow+"pm.tpl.ckd", "w")
+mudec.write(json.dumps(mutpl))
+mudec.close()
